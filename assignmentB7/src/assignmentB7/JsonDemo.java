06caab08c488672266e88da70a93026702d506a5
Ameykolhe <kolheamey99@gmail.com>
2018-09-25 23:04:22 +0530

    assignmentB7 Complete

--- assignmentB7/src/assignmentB7/JsonDemo.java
+++ assignmentB7/src/assignmentB7/JsonDemo.java
@@ -8,40 +8,72 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class JsonDemo{
 	
-	static BufferedReader br;
-	static BufferedWriter bw;
-	static {
-		try {
-			bw = new BufferedWriter(new FileWriter("jsonDemo.txt",true));
-			br = new BufferedReader(new FileReader("jsonDemo.txt"));
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+	static void decodeObjects() throws IOException, ParseException
+	{
+		BufferedReader br = new BufferedReader(new FileReader("jsonDemo.txt"));
+		
+		String str;
+		JSONObject ob;
+		int i = 1;
+		while( ( str = br.readLine() ) != null)
+		{
+			
+			System.out.println("Documnet NO :\t" + i + "\n");
+			
+			ob = (JSONObject)new  JSONParser().parse(str);
+			
+			Long rollNO = (Long)ob.get("RollNO");
+			System.out.println("RollNO      :\t" + rollNO.toString());
+			
+			JSONObject name = (JSONObject) ob.get("Name");
+			
+			String FName = (String)name.get("FName");
+			String LName = (String)name.get("LName");
+			System.out.println("Name        :  \tFName : " + FName);
+			System.out.println("\t\tLName : " + LName + "\n");
+			
+			JSONArray arr = (JSONArray) ob.get("Subject");
+			
+			String [] subjects = {"CN" , "DBMS" , "ISEE" , "SEPM" , "TOC"};
+			
+			System.out.println("Subject     :");
+			
+			for(int j = 0 ; j < arr.size() ; j++)
+			{
+				JSONObject subject = (JSONObject)arr.get(j);
+				Long marks = (Long)subject.get(subjects[j]);
+				
+				System.out.println("\t\t" + subjects[j] + "\t: " + marks);
+			}
+			
+			System.out.println("\n");
+			i+=1;
 		}
+		
+		br.close();
 	}
 	
-	void Close()
-	{
-		try {
-			br.close();
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	void addObject(Integer rollNO , String FName , String LName ) throws IOException
+	static void addObject(Integer rollNO , String FName , String LName ) throws IOException
 	{
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("jsonDemo.txt",true));
+		
 		JSONObject obj = new JSONObject();
 		
-		obj.put("RollNO", new Integer(3144));
+		obj.put("RollNO", rollNO);
 		
 		Map name = new HashMap<String , String>();
 		name.put("FName", FName);
@@ -64,13 +96,49 @@ public class JsonDemo{
 		
 		obj.put("Subject", ar);
 		
-		System.out.println(obj.toJSONString());
+		System.out.println("\n" + obj.toJSONString());
 		
 		bw.write(obj.toJSONString());
 		bw.newLine();
 		System.out.println("Write Successful");
 		
+		bw.close();
+		
 	}
 	
+	public static void main(String[] args) throws IOException, ParseException {
+		
+		int choice;
+		Scanner sc = new Scanner(System.in);
+		
+		String FName , LName;
+		Integer rollNO;
+		
+		while(true)
+		{
+			System.out.print("\n\nEnter Choice :\n1: Encode Object\n2: Decode Objects\n3: Exit\n>");
+			choice = sc.nextInt();
+			
+			if(choice == 1)
+			{
+				System.out.print("Enter RollNO      : ");
+				rollNO = Integer.parseInt(sc.next());
+				System.out.print("Enter First Name  : ");
+				FName = sc.next();
+				System.out.print("Enter Last Name   : ");
+				LName = sc.next();
+				JsonDemo.addObject(rollNO, FName, LName);
+			}
+			else if(choice == 2)
+			{
+				JsonDemo.decodeObjects();
+			}
+			else
+			{
+				sc.close();
+				break;
+			}
+		}
+	}
 	
 }
